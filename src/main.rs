f6f54bf044e87e77b39062f094a143826d2a70c0
Yosuke Kuroki <206260453+yosuke-kuroki@users.noreply.github.com>
2025-08-13 22:54:43 +0900

    feat: simple axum server

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,15 @@ use axum::{
     serve,
 };
 use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+use tower_http::cors::{Any, CorsLayer};
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3001));
+    print!("✔ Listening on {}", addr);
+
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
